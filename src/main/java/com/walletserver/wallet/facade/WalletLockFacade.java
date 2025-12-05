d36b72f0169315e15d686682e5e8c117bd183e15
leejaek <leejh9049@gmail.com>
2025-12-05 14:00:31 +0900

    [add] Redis Lock 및 Fallback 로직 추가

---
+++ src/main/java/com/walletserver/wallet/facade/WalletLockFacade.java
@@ -0,0 +1,51 @@
+package com.walletserver.wallet.facade;
+
+import com.walletserver.transaction.dto.WithdrawalRequest;
+import com.walletserver.transaction.dto.WithdrawalResponse;
+import com.walletserver.wallet.exception.LockAcquisitionException;
+import com.walletserver.wallet.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisConnectionException;
+import org.redisson.client.RedisTimeoutException;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WalletLockFacade {
+
+    private final RedissonClient redissonClient;
+    private final WalletService walletService;
+
+    public WithdrawalResponse withdraw(Long walletId, WithdrawalRequest req) {
+        String lockKey = "wallet:lock:" + walletId;
+        RLock lock = redissonClient.getLock(lockKey);
+
+        try {
+            boolean available = lock.tryLock(2, 3, TimeUnit.SECONDS);
+
+            if (!available) {
+                throw new LockAcquisitionException("잠시 후 다시 시도해주세요.");
+            }
+
+            try {
+                return walletService.withdraw(walletId, req, false);
+            } finally {
+                lock.unlock();
+            }
+
+        } catch (RedisConnectionException | RedisTimeoutException e) {
+            log.error("Redis 장애 감지! DB Lock으로 전환합니다. Error: {}", e.getMessage());
+            return walletService.withdraw(walletId, req, true);
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Server Interrupted");
+        }
+    }
+}
